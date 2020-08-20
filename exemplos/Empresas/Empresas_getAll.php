39b484299f1b90b1da365b8d9992bffbce8e49c0
frcgarcia@gmail.com <frcgarcia@gmail.com>
2020-08-20 08:35:29 -0300

    inicio

--- exemplos/Empresas/Empresas_getAll.php
+++ exemplos/Empresas/Empresas_getAll.php
@@ -2,7 +2,7 @@
     require __DIR__.'/../vendor/autoload.php';
     use SNFe\Empresas;        
     // Coloca o Token SNFe  no environment do PHP.
-    $snfe_token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJjb250YV9pZCI6Ijc1MCIsInVzZXIiOnsiaWQiOiI2NzAiLCJlbWFpbCI6ImFwaV80c2lzQHNiYXVtLmNvbS5iciIsInNlbmhhIjoiIzEyMzQifSwiZGF0YV9jcmlhZG8iOiIxOVwvMDhcLzIwMjAiLCJleHBpcmUiOiIwMVwvMzBcLzE5NzAifQ.n2j9wTFaRjzUoiXmyD09Dnmo-6Hs-2u42ccLH-5Ze8tCPf8R88kg-c16yIRflMsthHvwVfwlhDjFruNyfPHoRg";    
+    $snfe_token="";    
     putenv('SNFE_TOKEN='.$snfe_token);    
     // Define o ambiente no environment do PHP - esse ambiente não tem vinculo com Produção/Homologação na Sefaz.
     $snfe_env = "prod";    
